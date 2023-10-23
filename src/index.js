4d247e8fb78e5efacfbcfa95dc0914995968d190
hamzaesmael <hamzaesmael300@gmail.com>
2023-10-23 03:14:01 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const redis = require ('redis');
-const {client} = require ('pg');
+
 
 const PORT = process.env.PORT || 4000;
 const app = express ();
@@ -9,10 +9,7 @@ const app = express ();
 const REDIS_PORT = 6379;
 const REDIS_HOST = 'redis';
 
-const DB_USER = 'root';
-const DB_PASSWORD = 'example';
-const DB_PORT = 5432;
-const DB_HOST = 'postgres';
+
 
 
 const redisClient = redis.createClient({
@@ -22,14 +19,7 @@ redisClient.on('error',(err) => console.log('redis client error',err));
 redisClient.on('connect',() => console.log('connect to redis '));
 redisClient.connect();
 
-const URI = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
-const client = new Client({
-  connectionString: URI,
-});
-client
-  .connect(URI)
-  .then(() => console.log('connected to postgres..'))
-  .catch((err) => console.log('failed to connect to postgres db:',err));
+
 
 app.get('/',(req,res) => {
   redisClient.set('products', 'products...');
